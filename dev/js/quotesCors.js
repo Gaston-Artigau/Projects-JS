822a3619f7a86bc7e979c08751cf617deda3db5a
Gaston-Artigau <gastonartigau@gmail.com>
2022-04-17 12:06:18 -0300

    Update

---
+++ dev/js/quotesCors.js
@@ -0,0 +1,44 @@
+// var createError = require('http-errors');
+// var express = require('express');
+// var path = require('path');
+// var cookieParser = require('cookie-parser');
+// var logger = require('morgan');
+// var cors = require('cors');
+
+// var config = require('./config');
+
+// var createError = requirejs(['http-errors']);
+// var express = requirejs(['express']);
+// var path = requirejs(['path']);
+// var cookieParser = requirejs(['cookie-parser']);
+// var logger = requirejs(['morgan']);
+// const cors = requirejs(['cors']);
+
+// var config = requirejs(['./config']);
+
+// var app = express();
+
+// app.use(cors(
+//     config.application.cors.server
+// ));
+
+// var config = {
+//     application: {
+//         cors: {
+//             server: [{
+//                 origin: "*", //servidor que deseas que consuma o (*) en caso que sea acceso libre
+//                 credentials: true
+//             }]
+//         }
+//     }
+// }
+
+
+import express from '../../node_modules/express/index';
+// var express = require('express')
+import cors from '../../node_modules/cors/lib/index';
+// var cors = require('cors')
+
+const app = express()
+
+app.use(cors())
